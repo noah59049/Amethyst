531664a72ce8d04b51aa8355192110eedf2801b9
noah <ntholbro@uvm.edu>
2024-03-30 17:34:47 -0400

    Revert "QuietHistory now supports sorting std::array + refactoring"
    This reverts commit 7e98df3ad0367794e948f5d98e2f02a55152ec64.

--- search/QuietHistory.h
+++ search/QuietHistory.h
@@ -1,22 +1,20 @@
 #pragma once
 #include <vector>
-#include <array>
 #include "../Typedefs.h"
 
 
 class QuietHistory {
 private:
     int cutoffCounts[4096]{};
-    [[nodiscard]] inline int lookupMoveCutoffCount (const move_t move) const {
+    inline int lookupMoveCutoffCount (move_t move) {
         return cutoffCounts[move >> 4];
     }
 
-    void sortMovesByCutoffs(std::vector<move_t> &vec, unsigned int startIndex, unsigned int endIndex) const;
+    void sortMovesByCutoffs(std::vector<move_t> &vec, int startIndex, int endIndex);
 public:
     QuietHistory();
     void recordKillerMove (move_t move);
 
-    void sortMoves(std::vector<move_t>& quietMoves) const;
-    void sortMovesByCutoffs(std::array<move_t,218>& array, unsigned int startIndex, unsigned int endIndex) const;
+    void sortMoves(std::vector<move_t>& quietMoves);
 
 };
