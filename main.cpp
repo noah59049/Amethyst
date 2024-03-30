938e09afe4baa6dbf9728311bc41ea48d09dbb19
noah <ntholbro@uvm.edu>
2024-03-30 17:30:01 -0400

    Revert "This is the last commit before we revert because I have broken everything somehow"
    This reverts commit 9e6f963ed39a46684c4c459fa5f81835c41bf4fa.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include "movegen/ChessBoard.h"
 #include <iostream>
 #include <vector>
-#include "search/MoveOrder.h"
-#include "search/Negamax.h"
 using namespace std;
 const vector<vector<int>> EXPECTED_PERFT_COUNTS = {
         {}, // position 0
@@ -16,45 +14,19 @@ const vector<vector<int>> EXPECTED_PERFT_COUNTS = {
         {1,46,2079,89890,3894594,164075551} // position 6
 };
 int main() {
-//    ChessBoard board = ChessBoard::boardFromFENNotation("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
-//    for (int depth = 0; depth < 6; depth++) {
-//        auto start = std::chrono::high_resolution_clock::now();
-//        int numLegalMoves = board.perft(depth);
-//        int numExpectedLegalMoves = EXPECTED_PERFT_COUNTS.at(2).at(depth);
-//        auto end = std::chrono::high_resolution_clock::now();
-//        auto duration = std::chrono::duration_cast<chrono::milliseconds>(end - start);
-//        long ms = duration.count();
-//        if (numLegalMoves == numExpectedLegalMoves)
-//            cout << numLegalMoves << " legal moves at depth " << depth << " in " << ms << " milliseconds. Test succeeded." << endl;
-//        else
-//            cout << numLegalMoves << " legal moves at depth " << depth << ". Test failed. Expected " << numExpectedLegalMoves << ". Elapsed time: " << ms << " milliseconds." << endl;
-//    }
-
-//    float negaEval = board.getNegaStaticEval();
-//    for (int depth = 1; depth <= 6; depth++) {
-//        using namespace search;
-//        auto start = std::chrono::high_resolution_clock::now();
-//        bool* isCancelled = new bool(false);
-//        RepetitionTable repetitionTable;
-//        NegamaxData data(isCancelled,repetitionTable,depth);
-//        move_t bestMove = SEARCH_FAILED_MOVE_CODE;
-//        getNegamaxBestMoveAndEval(board,depth,data,negaEval,bestMove,negaEval);
-//        string readableBestMove = board.moveToSAN(bestMove);
-//        delete isCancelled;
-//        auto end = std::chrono::high_resolution_clock::now();
-//        auto duration = std::chrono::duration_cast<chrono::milliseconds>(end - start);
-//        long ms = duration.count();
-//        cout << "Nega eval is " << negaEval << " and best move is " << readableBestMove << " at depth " << depth << " in " << ms << " milliseconds." << endl;
-//    } // end for loop
-//
-//    MoveList legalMoves;
-//    board.getLegalMoves1(legalMoves);
-//    auto killerMoves = TwoKillerMoves();
-//    killerMoves.recordKillerMove(WHITE_SHORT_CASTLE);
-//
-//    sortMoves(legalMoves,board,WHITE_LONG_CASTLE,killerMoves,QuietHistory());
-//    for (move_t move : legalMoves)
-//        cout << board.moveToSAN(move) << endl;
-    uciLoop();
+    ChessBoard board = ChessBoard::boardFromFENNotation("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+    for (int depth = 0; depth < 6; depth++) {
+        auto start = std::chrono::high_resolution_clock::now();
+        int numLegalMoves = board.perft(depth);
+        int numExpectedLegalMoves = EXPECTED_PERFT_COUNTS.at(2).at(depth);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<chrono::milliseconds>(end - start);
+        long ms = duration.count();
+        if (numLegalMoves == numExpectedLegalMoves)
+            cout << numLegalMoves << " legal moves at depth " << depth << " in " << ms << " milliseconds. Test succeeded." << endl;
+        else
+            cout << numLegalMoves << " legal moves at depth " << depth << ". Test failed. Expected " << numExpectedLegalMoves << ". Elapsed time: " << ms << " milliseconds." << endl;
+    }
+//    uciLoop();
     return 0;
 }
--- search/MoveOrder.cpp
+++ search/MoveOrder.cpp
@@ -1,5 +1,4 @@
 #include "MoveOrder.h"
-#include <iostream>
 void sortMoves(MoveList& legalMoves, const ChessBoard &board, move_t hashMove, const TwoKillerMoves &killerMoves,
                          const QuietHistory &quietHistory) {
     // Step 1: Put the hash move first
@@ -25,7 +24,6 @@ void sortMoves(MoveList& legalMoves, const ChessBoard &board, move_t hashMove, c
             }
             else {
                 // the capture has negative SEE
-                std::cout << "capture " << board.moveToSAN(legalMoves.moveList[i]) << "has negative SEE" << std::endl;
                 move_t temp = legalMoves.moveList[backIndex];
                 legalMoves.moveList[backIndex] = legalMoves.moveList[i];
                 legalMoves.moveList[i] = temp;
