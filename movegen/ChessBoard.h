eb2a32f3d65ba53b10072db3894a72144b5a9e11
noah <ntholbro@uvm.edu>
2024-03-30 17:34:43 -0400

    Revert "Added support for getLegalMoves using MoveList class in ChessBoard"
    This reverts commit 61b4e0cf34b2a49d9ac83b69054adbe054625591.

--- movegen/ChessBoard.h
+++ movegen/ChessBoard.h
@@ -2,7 +2,6 @@
 #include <cstdint>
 #include <string>
 #include <vector>
-#include "../search/MoveList.h"
 #include "Flags.h"
 #include "../Typedefs.h"
 
@@ -58,9 +57,6 @@ private:
     void addCastling (std::vector<move_t>& legalMoves, bitboard_t enemyAttackedSquares) const;
     void addEnPassant (std::vector<move_t>& legalMoves, bitboard_t effectiveEnemyBishops, bitboard_t effectiveEnemyRooks) const;
     void addLegalKingMoves (std::vector<move_t>& legalMoves, bitboard_t kingLegalEndSquares) const;
-    void addCastling (MoveList& legalMoves, bitboard_t enemyAttackedSquares) const;
-    void addEnPassant (MoveList& legalMoves, bitboard_t effectiveEnemyBishops, bitboard_t effectiveEnemyRooks) const;
-    void addLegalKingMoves (MoveList& legalMoves, bitboard_t kingLegalEndSquares) const;
 
     // Part 5: SEE
     [[nodiscard]] int getCaptureSEE (int capturingPieceType, move_t captureMove) const;
@@ -86,7 +82,6 @@ public:
 
     // Part 2: Move generation
     void makemove(move_t move);
-    void getLegalMoves(MoveList& legalMoves) const;
     void getLegalMoves(std::vector<move_t>& legalMoves) const;
     void getLegalCapturesOnly (std::vector<move_t>& captures) const;
     void getNonnegativeSEECapturesOnly (std::vector<move_t>& captures) const;
