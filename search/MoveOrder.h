76852c19dd3573eedd8adae8ccf03f8b0bfed220
noah <ntholbro@uvm.edu>
2024-04-17 18:29:52 -0400

    Revert "Revert "Revert "Revert "Added conthist (and we are getting SIGBUS all the time)""""
    This reverts commit 60134e4bae26f1abeb6861709320e97df6eeca98.

--- search/MoveOrder.h
+++ search/MoveOrder.h
@@ -3,4 +3,6 @@
 #include "../movegen/ChessBoard.h"
 #include "QuietHistory.h"
 #include "TwoKillerMoves.h"
-void sortMoves(MoveList& legalMoves, const ChessBoard& board, move_t hashMove, const TwoKillerMoves& killerMoves, const QuietHistory& quietHistory);
+#include "Conthist.h"
+void sortMoves(MoveList& legalMoves, const ChessBoard &board, move_t hashMove, const TwoKillerMoves &killerMoves,
+               const QuietHistory &quietHistory, const Conthist& conthist, const std::vector<uint16_t>& conthistStack);
