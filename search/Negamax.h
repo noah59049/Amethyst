959c4eccc2d1213af92e15ba086b07cf5a79cc0a
noah <ntholbro@uvm.edu>
2024-06-25 22:52:18 -0700

    Changed futility reductions parameters to constexpr constants

--- search/Negamax.h
+++ search/Negamax.h
@@ -13,6 +13,8 @@ namespace search {
     constexpr const static unsigned int QUIETS_TO_NOT_REDUCE = 8;
     constexpr const static int SECOND_LMR_DEPTH = 7;
     constexpr const static unsigned int SECOND_QUIETS_TO_NOT_REDUCE = 5;
+    constexpr const static int MIN_FR_DEPTH = 3;
+    constexpr const static int FR_MARGIN = 256;
     constexpr const static eval_t ASPIRATION_WINDOW_RADIUS = 75;
     constexpr const static eval_t RFP_MARGIN = 150;
     constexpr const static int MAX_RFP_DEPTH = 4;
