724f049ea3d322135eac6fc95a492cc05237b9e2
noah <ntholbro@uvm.edu>
2024-12-30 12:57:53 -0800

    bug fix: cap history in the range [-512, 511] instead of [-512, 1023], preventing overflow issues

--- search.cpp
+++ search.cpp
@@ -232,7 +232,7 @@ eval_t negamax(sg::ThreadData& threadData, const ChessBoard& board, depth_t dept
             const auto fromTo = mvs::getFromTo(bestMove);
             threadData.butterflyHistory[stm][fromTo] = std::min(
                     threadData.butterflyHistory[stm][fromTo] +
-                    history_t(depth) * history_t(depth), 1023);
+                    history_t(depth) * history_t(depth), 511);
         } // end if best move is quiet
         
         // Step 15B: malus to all moves before this that didn't cause a cutoff
