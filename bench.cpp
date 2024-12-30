c5b9fa6375a794f77b4a8c6f8b53be751b77cef8
noah <ntholbro@uvm.edu>
2024-12-29 21:46:05 -0800

    increase bench depth to 10

--- bench.cpp
+++ bench.cpp
@@ -40,7 +40,7 @@ const std::string BENCH_POSITIONS[] = {
 };
 
 perft_t bench() {
-    sg::depthLimit = 8;
+    sg::depthLimit = 10;
     sg::nodesLimit = INT64_MAX;
     sg::hardTimeLimit = 1000000000;
     sg::softTimeLimit = 1000000000;
