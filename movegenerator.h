56939f2bc6ba56d2965e4d221874591c1a536941
noah <ntholbro@uvm.edu>
2025-01-01 10:28:49 -0800

    added stuff from previous staged movegen branch

--- movegenerator.h
+++ movegenerator.h
@@ -6,11 +6,46 @@
 
 
 
-class MovePicker {
+class MoveGenerator {
 private:
-    std::array<move_t, 256> moves;
-    const ChessBoard board;
+    MoveList moves;
+    ChessBoard board;
+    size_t nextMoveIndex;
+    MovegenStage stage;
+
+    move_t nextPseudolegalMove() {
+        if (nextMoveIndex == moves.size) {
+            switch (stage) {
+                case TACTICALS: {
+                    nextMoveIndex = 0;
+                    stage = QUIETS;
+                    moves = board.getMoves(stage);
+                    return nextPseudolegalMove();
+                }
+                case QUIETS: {
+                    return 0;
+                } // end case QUIETS
+            } // end switch
+        } // end if nextMoveIndex == moves.size
+        else {
+            return moves.at(nextMoveIndex++);
+        }
+    } // end nextMove method
 public:
-    explicit MovePicker(const ChessBoard &board);
-    void add(move_t move, MovegenStage stage);
-};
+    explicit MovePicker(const ChessBoard &board1) : board(board1) {
+        nextMoveIndex = 0;
+        stage = TACTICALS;
+        moves = board.getMoves(stage);
+        scoreMovesByMVVLVA(moves);
+        std::sort(moves.begin(), moves.end(), std::greater<>());
+    }
+
+    move_t nextMove() {
+        move_t move;
+        do {
+            move = nextPseudolegalMove();
+        }
+        while (move != 0 and !board.isLegal(move));
+        return move;
+    }
+}; // end class
